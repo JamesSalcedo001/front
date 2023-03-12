28030f1ba3e69341319e53e874f5ffd14ee95a45
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-03-12 00:30:34 -0600

    added some simple components, header and form

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+function Form() {
+    return (
+        <div>
+            <form>
+                <input placeholder="type shit here"/>
+                <button type="submit">Add shit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
---
+++ src/Header.js
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+        <h1>Hey whaddup</h1>
+    )
+}
+
+export default Header
