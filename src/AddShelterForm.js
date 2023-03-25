2bd493a63a4cd3a9ca38b1230e5641edaf80ce70
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-03-24 23:47:11 -0500

    made shelter form controlled with state

--- src/AddShelterForm.js
+++ src/AddShelterForm.js
@@ -1,9 +1,34 @@
+import {useState} from "react"
+
 function AddShelterForm() {
+    const [formData, setFormData] = useState({
+        name: "",
+        location: "",
+        employees: "",
+        image: ""
+    })
+
+    function changeHandler() {
+        const {name, value} = e.target
+        setFormData((formDataObj) => ({...formDataObj, [name]:value }))
+    }
+
     return (
         <div>
             <form>
-                <input placeholder="type stuff here"/>
-                <button type="submit">Add stuff</button>
+                <label htmlFor="name"></label>
+                <input onChange={changeHandler} value={formData.name} placeholder="shelter name"/>
+
+                <label htmFor="location"></label>
+                <input onChange={changeHandler} value={formData.location} placeholder="shelter location"/>
+
+                <label htmlFor="employees"></label>
+                <input onChange={changeHandler} value={formData.employees} placeholder="number of employees"/>
+
+                <label htmlFor="image"></label>
+                <input onChange={changeHandler} value={formData.image} placeholder="shelter image"/>
+
+                <button type="submit">Add Shelter</button>
             </form>
         </div>
     )
