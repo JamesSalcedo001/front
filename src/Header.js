335db61be1e3d1cf80166926e5d95f2a7fe66f4f
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-03-28 23:43:38 -0500

    changed some css in the header

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,13 @@
 import {NavLink} from "react-router-dom"
 function Header() {
     return (
-        <header id="headerSection">
-            <nav>
-                <h1>Kitty Shelter</h1>
-                <NavLink exact to="/cats">Cats</NavLink>
-                <NavLink exact to="/addcatform">Add Cats</NavLink>
-                <NavLink exact to="/addshelterform">Add Shelters</NavLink>
-                <NavLink exact to="/shelters">Shelters</NavLink>
+        <header>
+            <nav id="headerSection">
+                <h1 id="title">Kitty Shelter</h1>
+                <NavLink className="navButtons" exact to="/cats">Cats List</NavLink>
+                <NavLink className="navButtons" exact to="/addcatform">Add Cats</NavLink>
+                <NavLink className="navButtons" exact to="/addshelterform">Add Shelters</NavLink>
+                <NavLink className="navButtons" exact to="/shelters">Shelters</NavLink>
             </nav>
         </header>
     )
