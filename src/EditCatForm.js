eb5af5c169be7237b729d4aa41ee8e0f0eb33ccf
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-03-28 21:11:55 -0500

    added a change handler function to handle the onchange events in the inputs

--- src/EditCatForm.js
+++ src/EditCatForm.js
@@ -5,27 +5,32 @@ function EditCatForm({cats}) {
 
     const [formData, setFormData] = useState(cats)
 
+    function changeHandler(e) {
+        const { name, value } = e.target
+        setFormData((formDataObj) => ({...formDataObj, [name]:value }))
+    }
+
     return (
         <div>
             <section>
                 <form>
                     <label>Name</label>
-                    <input value={name} onChange={} type="text" name="name" placeholder="name"/>
+                    <input value={name} onChange={changeHandler} type="text" name="name" placeholder="name"/>
 
                     <label>Breed</label>
-                    <input value={breed} onChange={} type="text" name="breed" placeholder="breed"/>
+                    <input value={breed} onChange={changeHandler} type="text" name="breed" placeholder="breed"/>
 
                     <label>Age</label>
-                    <input value={age} onChange={} type="text" name="age" placeholder="age"/>
+                    <input value={age} onChange={changeHandler} type="text" name="age" placeholder="age"/>
 
                     <label>Description</label>
-                    <input value={descr} onChange={} type="text" name="description" placeholder="description"/>
+                    <input value={descr} onChange={changeHandler} type="text" name="description" placeholder="description"/>
 
                     <label>Image</label>
-                    <input value={image} onChange={} type="text" name="image" placeholder="image"/>
+                    <input value={image} onChange={changeHandler} type="text" name="image" placeholder="image"/>
 
                     <label>Shelter ID</label>
-                    <input value={shelter_id} onChange={} type="number" step="1" name="shelter_id" placeholder="shelter_id"/>
+                    <input value={shelter_id} onChange={changeHandler} type="number" step="1" name="shelter_id" placeholder="shelter_id"/>
 
                     <button type="submit">Update Cat</button>
 
