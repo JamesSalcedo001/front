d0302df654d6a0bb500e0b0a00666159af7202aa
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-04-16 17:04:43 -0500

    changed the forms, passed the shelter state down into the add cat form, mapped through shelters and used the options list of shelter names as a way to assign a cat to a shelter.

--- src/AddCatForm.js
+++ src/AddCatForm.js
@@ -79,3 +79,92 @@
 // }
 
 // export default AddCatForm;
+
+
+
+
+import {useState} from "react"
+import {useHistory} from "react-router-dom"
+
+function AddCatForm({addCat, shelters}) {
+    const [formData, setFormData] = useState({
+        cat_name: "",
+        age: "",
+        breed: "",
+        descr: "",
+        cat_image: "",
+        shelter_id: "",
+    })
+
+    const history = useHistory()
+
+
+    function changeHandler(e) {
+        const {name, value} = e.target
+        setFormData((formDataObj) => ({...formDataObj, [name]:value}))
+    }
+
+    function submitCatForm(e) {
+        e.preventDefault()
+
+        const configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        }
+
+        fetch("http://localhost:9292/cats", configObj)
+        .then((res) => res.json())
+        .then((newCat) => {
+            addCat(newCat)
+            setFormData({
+                cat_name: "",
+                age: "",
+                breed: "",
+                descr: "",
+                cat_image: "",
+                shelter_id: "",
+            })
+            history.push("/")
+        })
+    }
+
+
+    const shelterList = shelters.map(shelter =>
+        <option key={shelter.id} value={shelter.id}>{shelter.name}</option>
+        )
+
+
+
+    return (
+        <div>
+            <section>
+                <form className="formStyle" onSubmit={submitCatForm}>
+                    <label htmlFor="name">Cat Name</label>
+                    <input className="formInput" type="text" name="cat_name" value={formData.cat_name} onChange={changeHandler} placeholder="name" />
+
+                    <label htmlFor="breed">Cat Breed</label>
+                    <input className="formInput" type="text" name="breed" value={formData.breed} onChange={changeHandler} placeholder="breed" />
+
+                    <label htmlFor="age">Cat Age</label>
+                    <input className="formInput" type="number" name="age" value={formData.age} onChange={changeHandler} placeholder="age"/>
+
+                    <label htmlFor="descr">Cat Description</label>
+                    <input className="formInput" type="text" name="descr" value={formData.descr} onChange={changeHandler} placeholder="description"/>
+
+                    <label htmlFor="image">Cat ImageURL</label>
+                    <input className="formInput" type="text" name="cat_image" value={formData.cat_image} onChange={changeHandler} placeholder="image"/>
+
+                    <label htmlFor="shelter_id">Closest Shelter</label>
+                    <select className="formInput" name="shelter_id" value={formData.shelter_id} onChange={changeHandler}>{shelterList}</select>
+                    
+                    <input className="formInput" type="submit" value="Add Cat"/>
+                </form>
+            </section>
+        </div>
+    )
+}
+
+export default AddCatForm;
